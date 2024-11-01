ed2a012729ebf7fa1664363efbab4400924a56fa
ernarrr <ernar@gmail.com>
2024-11-01 17:56:13 +0500

    class napitkov

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+public abstract class Beverage
+{
+    public abstract string GetDescription();
+    public abstract double Cost();
+}
+public class Espresso : Beverage
+{
+    public override string GetDescription() => "Espresso";
+    public override double Cost() => 1.5;
+
+}
+public class Tea : Beverage
+{
+    public override string GetDescription() => "Espresso";
+    public override double Cost() => 1.5;
+
+}
+public class Latte : Beverage
+{
+    public override string GetDescription() => "Latte";
+    public override double Cost() => 2.0;
+}
