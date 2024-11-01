c412d28bcd14d0d1fe7553a5d64e4239b39930d2
ernarrr <ernar@gmail.com>
2024-11-01 18:22:41 +0500

    sozdanie napitkov

--- Program.cs
+++ Program.cs
@@ -22,3 +22,54 @@ public class Latte : Beverage
     public override string GetDescription() => "Latte";
     public override double Cost() => 2.0;
 }
+public abstract class BeverageDecorator : Beverage
+{
+    protected Beverage _beverage;
+
+    public BeverageDecorator(Beverage beverage)
+    {
+        _beverage = beverage;
+    }
+    public override string GetDescription() => _beverage.GetDescription();
+    public override double Cost() => _beverage.Cost();
+}
+
+public class Milk : BeverageDecorator
+{
+    public Milk(Beverage beverage) : base(beverage) { }
+    public override string GetDescription() => _beverage.GetDescription() + ", Milk";
+    public override double Cost() => _beverage.Cost() + 0.3;
+}
+public class Sugar : BeverageDecorator
+{
+    public Sugar(Beverage beverage) : base(beverage) { }
+    public override string GetDescription() => _beverage.GetDescription() + ", Sugar";
+    public override double Cost() => _beverage.Cost() + 0.2;
+}
+
+public class WhippedCream : BeverageDecorator
+{
+    public WhippedCream(Beverage beverage) : base(beverage) { }
+    public override string GetDescription() => _beverage.GetDescription() + ", Whipped Cream";
+    public override double Cost() => _beverage.Cost() + 0.5;
+}
+public class Syrup : BeverageDecorator
+{
+    public Syrup(Beverage beverage) : base(beverage) { }
+    public override string GetDescription() => _beverage.GetDescription() + ", Syrup";
+    public override double Cost() => _beverage.Cost() + 0.4;
+}
+public class CafeOrder
+{
+    public static void CreateOrder()
+    {
+        Beverage beverage= new Beverage();
+        beverage = new Milk(beverage);
+        beverage = new Sugar(beverage);
+        beverage= new WhippedCream(beverage);
+
+        Console.WriteLine($"Order: {beverage.GetDescription()}");
+        Console.WriteLine($"Total Cost: ${beverage.Cost():0.00}");
+    }
+}
+
